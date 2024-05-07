5f90a534e81cca1f4a660405ac292bd0357601cc
osamuel-cardoso <168598138+resolvoo@users.noreply.github.com>
2024-05-07 20:56:09 -0300

    updated

--- src/services/hygraph/getPosts.query.ts
+++ src/services/hygraph/getPosts.query.ts
@@ -6,7 +6,7 @@ export async function getPosts({ size }: { size: number }) {
     const { data, loading, error } = await apolloClient.query({
         query: gql`
             query getSinglePost($size: Int!) {
-                posts(first: $size, orderBy:publishedAt_DESC) {
+                posts(first: $size, orderBy: publishedAt_DESC) {
                     url
                     titulo
                     descricao
