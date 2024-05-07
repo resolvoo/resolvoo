902eea8dca625ae5d1795da523cfd10e6f41c0e2
osamuel-cardoso <168598138+resolvoo@users.noreply.github.com>
2024-05-07 20:50:42 -0300

    updated

--- src/services/hygraph/getPosts.query.ts
+++ src/services/hygraph/getPosts.query.ts
@@ -5,7 +5,7 @@ import { apolloClient } from '@/lib/apollo/apollo.client'
 export async function getPosts({ size }: { size: number }) {
     const { data, loading, error } = await apolloClient.query({
         query: gql`
-            query getSinglePost($size: Int!, ) {
+            query getSinglePost($size: Int!) {
                 posts(first: $size, orderBy:publishedAt_DESC) {
                     url
                     titulo
